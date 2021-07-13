a8f3c52641214337686c6a724702c00c6fe9e4ce
guisalmeida <guilherme.dasilva@linx3.com>
2021-07-13 18:38:25 -0300

    feat: add mask binary

--- classes/pyipv4.py
+++ classes/pyipv4.py
@@ -21,15 +21,30 @@ class PyIpv4:
 
     @ip.setter
     def ip(self, ip):
-        self._ip_validate(ip)
+        if not self._ip_validate(ip):
+            raise ValueError('Ip inválido!')
         self._ip = ip
 
     @mask.setter
     def mask(self, mask):
+        if not mask:
+            return
+        if not self._ip_validate(mask):
+            raise ValueError('Máscara inválida!')
         self._mask = mask
+        self._mask_bin = self._ip_to_bin(mask)
 
     @cidr.setter
     def cidr(self, cidr):
+        if not cidr:
+            return
+
+        if not isinstance(cidr, int):
+            raise TypeError('Cidr precisa ser um número inteiro.')
+
+        if cidr > 32:
+            raise ValueError('Cidr excede limite de 32Bits.')
+
         self._cidr = cidr
 
     @staticmethod
@@ -37,5 +52,11 @@ class PyIpv4:
         regexp = re.compile(
             r'^([0-9]{1,3}\.?){4}$'
         )
+        if regexp.search(ip):
+            return True
 
-        print(regexp.search(ip))
+    @staticmethod
+    def _ip_to_bin(ip):
+        blocs = ip.split('.')
+        octets = [bin(int(bloc))[2:].zfill(8) for bloc in blocs]
+        return ''.join(octets)
